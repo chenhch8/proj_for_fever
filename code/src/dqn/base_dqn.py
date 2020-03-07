b7acdcb6f3a6630a97bae99ade4fdede129303cf
chenhch8 <chenhch8@mail2.sysu.edu.cn>
2020-03-07 10:11:18 +0800

    max_seq_length改为batch内最大长度

--- code/src/dqn/base_dqn.py
+++ code/src/dqn/base_dqn.py
@@ -136,19 +136,19 @@ class BaseDQN:
         if is_eval: net.eval()
         
         q_values = None
+        with torch.no_grad():
+            inputs = self.convert_to_inputs_for_select_action(state, actions)
+            q_values = [net(
+                **dict(map(lambda x: (x[0], x[1].to(self.device)), clip_inputs.items()))
+            )[0] for clip_inputs in inputs]
+            q_values = torch.cat(q_values, dim=0)
         # epsilon greedy
         sample = random.random()
         eps_threshold = self.eps_end + (self.eps_start - self.eps_end) * \
             math.exp(-1. * self.steps_done / self.eps_decay)
         self.steps_done += 1 if not is_eval else 0
         if sample > eps_threshold or is_eval:
-            with torch.no_grad():
-                inputs = self.convert_to_inputs_for_select_action(state, actions)
-                q_values = [net(
-                    **dict(map(lambda x: (x[0], x[1].to(self.device)), clip_inputs.items()))
-                )[0] for clip_inputs in inputs]
-                q_values = torch.cat(q_values, dim=0)
-                max_action = q_values.argmax().cpu().data.item()
+            max_action = q_values.argmax().cpu().data.item()
             sent_id = max_action // self.args.num_labels
             label_id = max_action % self.args.num_labels
         else:
@@ -156,7 +156,7 @@ class BaseDQN:
             sent_id = random.randint(0, len(actions) - 1)
             label_id = random.randint(0, self.args.num_labels - 1)
         action = Action(sentence=actions[sent_id].sentence, label=label_id)
-        q = q_values[sent_id, label_id].cpu().data.item() if q_values is not None else None
+        q = q_values[sent_id, label_id].cpu().data.item()
         return action, q
 
 
