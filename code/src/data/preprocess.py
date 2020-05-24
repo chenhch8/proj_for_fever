7b86db9f805fbf9201a6de711ea1c664ac538724
chenhch8 <chenhch8@mail2.sysu.edu.cn>
2020-05-24 12:07:35 +0800

    test数据预处理

--- code/src/data/preprocess.py
+++ code/src/data/preprocess.py
@@ -18,14 +18,21 @@ def normalize(text: str) -> str:
     """Resolve different type of unicode encodings."""
     return unicodedata.normalize('NFD', text)
 
-def data_process(in_file: str, out_file: str, is_train: bool=True) -> None:
+def data_process(in_file: str, out_file: str) -> None:
+    if in_file.find('train') != -1:
+        mode = 'train'
+    elif in_file.find('dev') != -1:
+        mode = 'dev'
+    else:
+        mode = 'test'
     print(f'Loading {in_file}')
     instances = []
     with open(in_file, 'rb') as fr:
         for line in tqdm(fr.readlines()):
             instance = json.loads(line.decode(ENCODING).strip('\r\n'))
             
-            if is_train:
+            if mode == 'train':
+                label = instance['label']
                 evidence_set = []
                 for evidence in instance['evidence']:
                     process_evidence = []
@@ -35,11 +42,16 @@ def data_process(in_file: str, out_file: str, is_train: bool=True) -> None:
                         process_evidence.append([sent[2], sent[3]])
                     if len(process_evidence) and process_evidence not in evidence_set:
                         evidence_set.append(process_evidence)
-            else: evidence_set = instance['evidence']
+            elif mode == 'dev':
+                label = instance['label']
+                evidence_set = instance['evidence']
+            else:
+                label = None
+                evidence_set = None
 
             instances.append({
                 'id': instance['id'],
-                'label': instance['label'],
+                'label': label,
                 'claim': instance['claim'],
                 'evidence_set': evidence_set,
                 'predicted_pages': instance['predicted_pages']
@@ -49,7 +61,7 @@ def data_process(in_file: str, out_file: str, is_train: bool=True) -> None:
     with open(out_file, 'wb') as fw:
         for instance in tqdm(instances):
             titles = instance['predicted_pages']
-            if is_train:
+            if mode == 'train':
                 titles += [title for evidence in instance['evidence_set'] for title, _ in evidence]
             titles = list(set(titles))
             documents = defaultdict(dict)
@@ -82,6 +94,6 @@ def data_process(in_file: str, out_file: str, is_train: bool=True) -> None:
             }) + '\n').encode(ENCODING))
 
 if __name__ == '__main__':
-    data_process('./data/retrieved/train.wiki7.jsonl', './data/dqn/train.jsonl', is_train=True)
-    data_process('./data/retrieved/dev.wiki7.jsonl', './data/dqn/dev.jsonl', is_train=False)
-    #data_process('./data/retrieved/test.wiki7.jsonl', './data/dqn/test.jsonl', is_train=False)
+    data_process('./data/retrieved/train.wiki7.jsonl', './data/dqn/train.jsonl')
+    data_process('./data/retrieved/dev.wiki7.jsonl', './data/dqn/dev.jsonl')
+    data_process('./data/retrieved/test.wiki7.jsonl', './data/dqn/test.jsonl')
