e127a25426e129b0b2cd851f085b8bd30cb121d0
chenhch8 <chenhch8@mail2.sysu.edu.cn>
2020-07-16 11:48:06 +0800

    修改新reward

--- code/src/environment.py
+++ code/src/environment.py
@@ -20,8 +20,7 @@ class BaseEnv:
         return State(claim=state.claim,
                      label=state.label,
                      evidence_set=state.evidence_set,
-                     candidate=state.candidate + [action.sentence] \
-                                if action.sentence is not None else state.candidate,
+                     candidate=state.candidate + [action.sentence],
                      pred_label=action.label,
                      count=state.count + 1)
     
@@ -57,6 +56,9 @@ class ChenEnv(BaseEnv):
             cond2 = any([action.sentence in evi for evi in state.evidence_set])
 
         if state.label == self.label2id['NOT ENOUGH INFO']: # N
+            if cond1: return 1
+            elif self.is_done(state): return -1
+            else: return 0
             return 1. if cond1 else -1.
         else: # T/F
             #return 1. if cond1 and cond2 else -1
@@ -64,12 +66,10 @@ class ChenEnv(BaseEnv):
                 return 1.
             elif cond1 and not cond2:
                 return 0.
-            elif not cond1 and cond2:
+            elif self.is_done(state):
                 return -1.
-            elif not (cond1 or cond2):
-                return -2.
             else:
-                return ValueError('condition error')
+                return 0.
 
     def step(self, state: State, action: Action) -> Tuple[State, float, bool]:
         state_next = BaseEnv.new_state(state, action)
